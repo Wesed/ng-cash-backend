82dd3bd454f122a3854b9de5207fa078ec71b977
Wesed <dev.wfeduardo@gmail.com>
2022-11-23 16:53:07 -0300

    Modificando o cors

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,19 @@ const cors = require('cors');
 const app = express();
 AppDataSource.initialize();
 
-const whitelist = ["http://localhost:3000", "https://ngcash-backend.herokuapp.com/"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
+// const whitelist = ["http://localhost:3000", "https://ngcash-backend.herokuapp.com/"]
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+//   credentials: true,
+// }
+// app.use(cors(corsOptions))
+app.use(cors())
 
 
 app.use(bodyParser.json());
